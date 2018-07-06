46df2fe3021da479fb6b696b1def9e28f34f293d
grilledchickenthighs <pauldmendes5@gmail.com>
2018-07-06 15:55:44 -0400

    first commit

---
+++ telegram_marketpulse.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Simple Bot to reply to Telegram messages
+# This program is dedicated to the public domain under the CC0 license.
+"""
+This Bot uses the Updater class to handle the bot.
+First, a few callback functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+Usage:
+Example of a bot-user conversation using ConversationHandler.
+Send /start to initiate the conversation.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
+from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler,
+                          ConversationHandler)
+
+import logging
+import requests
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+PAIR, PHOTO, LOCATION, BIO = range(4)
+
+
+def start(bot, update):
+    # reply_keyboard = [['BTC', 'ETH', 'Other']]
+    update.message.reply_text(
+        'Hi! Welcome to Market Paulse! Let me help you analyze the markets. '
+        'Send /cancel to stop talking to me.\n\n'
+        'Send your crypto currency pair ie. "BTC"')
+        # reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+
+    return PAIR
+
+
+def pair(bot, update):
+    user = update.message.from_user
+    logger.info("Pair of %s: %s", user.first_name, update.message.text)
+    # data = price(update.message.text)
+    # update.message.reply_text('I see! Here is the OHLCV for pair, if youd like to '
+    #                           'be alerted and know when big money is buying or selling,'
+    #                           'just subsribe to this bot. '
+    #                           'so I know what you look like, or send /skip if you don\'t want to.',
+    #                           'I hope you like ' data
+    #                           reply_markup=ReplyKeyboardRemove())
+    # sleep(1)
+    update.message.reply_text(price(update.message.text).items()[0][1])#, reply_markup=ReplyKeyboardRemove())
+    return PAIR
+
+def price(symbol, comparison_symbols=['USD'], exchange=''):
+    url = 'https://min-api.cryptocompare.com/data/price?fsym={}&tsyms={}'\
+            .format(symbol.upper(), ','.join(comparison_symbols).upper())
+    if exchange:
+        url += '&e={}'.format(exchange)
+    page = requests.get(url)
+    data = page.json()
+    return data
+
+
+def photo(bot, update):
+    user = update.message.from_user
+    photo_file = bot.get_file(update.message.photo[-1].file_id)
+    photo_file.download('user_photo.jpg')
+    logger.info("Photo of %s: %s", user.first_name, 'user_photo.jpg')
+    update.message.reply_text('Gorgeous! Now, send me your location please, '
+                              'or send /skip if you don\'t want to.')
+
+    return LOCATION
+
+
+def skip_photo(bot, update):
+    user = update.message.from_user
+    logger.info("User %s did not send a photo.", user.first_name)
+    update.message.reply_text('I bet you look great! Now, send me your location please, '
+                              'or send /skip.')
+
+    return LOCATION
+
+
+def location(bot, update):
+    user = update.message.from_user
+    user_location = update.message.location
+    logger.info("Location of %s: %f / %f", user.first_name, user_location.latitude,
+                user_location.longitude)
+    update.message.reply_text('Maybe I can visit you sometime! '
+                              'At last, tell me something about yourself.')
+
+    return BIO
+
+
+def skip_location(bot, update):
+    user = update.message.from_user
+    logger.info("User %s did not send a location.", user.first_name)
+    update.message.reply_text('You seem a bit paranoid! '
+                              'At last, tell me something about yourself.')
+
+    return BIO
+
+
+def bio(bot, update):
+    user = update.message.from_user
+    logger.info("Bio of %s: %s", user.first_name, update.message.text)
+    update.message.reply_text('Thank you! I hope we can talk again some day.')
+
+    return ConversationHandler.END
+
+
+def cancel(bot, update):
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    update.message.reply_text('Bye! I hope we can talk again some day.',
+                              reply_markup=ReplyKeyboardRemove())
+
+    return ConversationHandler.END
+
+
+def error(bot, update, error):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+def main():
+    # Create the EventHandler and pass it your bot's token.
+    updater = Updater("311802806:AAG3n5MvKGBlyvYUVlieUvg0uR9P2a9geas")
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+
+        states={
+            # PAIR: [RegexHandler('^(BTC|ETH|LTC)$', pair)],
+            PAIR: [MessageHandler(Filters.text, pair)],
+
+            PHOTO: [MessageHandler(Filters.photo, photo),
+                    CommandHandler('skip', skip_photo)],
+
+            LOCATION: [MessageHandler(Filters.location, location),
+                       CommandHandler('skip', skip_location)],
+
+            BIO: [MessageHandler(Filters.text, bio)]
+        },
+
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
+
+    dp.add_handler(conv_handler)
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
