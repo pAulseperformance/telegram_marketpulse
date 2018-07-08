4b4aeb0ec0f1105ab190c7b0d68e40c67fc3a68c
grilledchickenthighs <pauldmendes5@gmail.com>
2018-07-08 03:30:18 -0400

    Restructuring program architecture

--- telegram/telegram_data_messenger.py
+++ telegram/telegram_data_messenger.py
@@ -9,15 +9,14 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 plt.style.use('ggplot')
 
-# import cryptocompare_wrapper as ccw
+import importlib as imp
+imp.reload(analysis)
 
 # Telegram API keys
 Token = tele_keys.Token
 MYChannelID = tele_keys.MYChannelID
 BOT = telegram.Bot(token=Token)
 # BOT.sendMessage(MYChannelID,'test')
-
-
 
 def save_plot(symbol):
     df = ccw.daily_price_historical(symbol)
@@ -38,6 +37,7 @@ BOT.sendMessage(MYChannelID, message)
 # expand to all coins
 # for coin in coinlist:
 # poll_volume_alert(coin)
+
 def main():
     BOT.sendMessage(MYChannelID, poll_volume_day('btc'))
 
